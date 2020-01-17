63e0c7efcc44b966e957cf3b0f100e415f21a2e2
Mojjan4 <Mojjan4@users.noreply.github.com>
2020-01-17 11:08:42 +0100

    Initial Commit

---
+++ labb1.ChristopherPersson/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  labb1.ChristopherPersson
+//
+//  Created by Christopher Persson on 2020-01-17.
+//  Copyright © 2020 Christopher Persson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
